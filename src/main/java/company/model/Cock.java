2bfbc2d57dc423bf78e481b7a4f7f98f81a1ff1e
DoloresDai <841856753@qq.com>
2019-02-20 05:58:47 +0800

    Create class cock that rewrite the method of obtaining prices.

---
+++ src/main/java/company/model/Cock.java
@@ -0,0 +1,10 @@
+package main.java.company.model;
+
+public class Cock extends Chicken {
+    public Cock(String name){
+        super(name);
+    }
+    public double getPrice(String name) {
+        return 5;
+    }
+}
