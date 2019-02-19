ff78873a045ab64939812d204e0df50f4a76afd5
DoloresDai <841856753@qq.com>
2019-02-20 05:59:59 +0800

    Create a tool class to set chicken properties.

---
+++ src/main/java/company/tools/SetChicken.java
@@ -0,0 +1,21 @@
+package main.java.company.tools;
+
+import main.java.company.model.Chicken;
+
+public class SetChicken {
+    public static void setChickenNumer(Chicken chicken, int sumNumber) {
+        chicken.setNumber((int) ((sumNumber) / getChickenPrice(chicken)));
+    }
+
+    public static double getChickenPrice(Chicken chicken) {
+        return chicken.getPrice(chicken.name);
+    }
+
+    public static String getChickenName(Chicken chicken) {
+        return chicken.getName();
+    }
+
+    public static int getChickenNumber(Chicken chicken) {
+        return chicken.getNumber();
+    }
+}
