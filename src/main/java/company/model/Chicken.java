7c1a9db436a9abf2e44876700ddb90be21eaacb3
DoloresDai <841856753@qq.com>
2019-02-20 05:57:46 +0800

    Create an abstract chicken-like chicken with name, price, and settings.

---
+++ src/main/java/company/model/Chicken.java
@@ -0,0 +1,20 @@
+package main.java.company.model;
+
+public abstract class Chicken {
+    private int number=1;
+    public String name;
+    Chicken(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setNumber(int number){
+        this.number=number;
+    }
+    public int getNumber(){
+        return this.number;
+    }
+    public abstract double getPrice(String name);
+
+}
