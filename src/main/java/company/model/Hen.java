0a2eca014175751591bd2f0e3f8a444e6438733a
DoloresDai <841856753@qq.com>
2019-02-20 05:59:03 +0800

    Create class hen that rewrite the method of obtaining prices.

---
+++ src/main/java/company/model/Hen.java
@@ -0,0 +1,10 @@
+package main.java.company.model;
+
+public class Hen extends Chicken{
+    public Hen(String name){
+        super(name);
+    }
+    public double getPrice(String name) {
+        return 3;
+    }
+}
